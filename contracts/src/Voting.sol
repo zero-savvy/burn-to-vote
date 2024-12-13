ecc920d6ae19f46405d9a649466c6a82389c5e48
Pardis <pardis@Pardiss-MacBook-Air.local>
2024-12-13 21:16:23 +0100

    Feature(core): update contract .

--- contracts/src/Voting.sol
+++ contracts/src/Voting.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.13;
 
 contract Voting {
 
-
-    function submitVote() public {}
+    uint16 public totalVotes;
+    function submitVote(uint8 _vote) public {
+        require(_vote == 0 || _vote == 1, "Invalid vote");
+        totalVotes = totalVotes + _vote;
+        
     }
-
-    function votingResult() public {}
 }
