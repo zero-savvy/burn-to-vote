f3fdd58af4b838b8f5b6ba7cf6cd04f37fd6efc8
Pardis <pardis@Pardiss-MacBook-Air.local>
2024-12-13 18:27:18 +0100

    Feature(cicuit): add base contract.

---
+++ contracts/src/Voting.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Voting {
+
+
+    function submitVote() public {}
+    }
+
+    function votingResult() public {}
+}
---
+++ contracts/test/Voting.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Voting} from "../src/Voting.sol";
+
+contract VotingTest is Test {
+
+}
