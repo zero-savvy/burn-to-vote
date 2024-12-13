ba6374325a415f3d676ce7e17fb056ce7fa100ac
Pardis <pardis@Pardiss-MacBook-Air.local>
2024-12-13 17:20:27 +0100

    Feature(cicuit): add commands.

---
+++ src/commands/burn.rs
@@ -0,0 +1,3 @@
+pub async fn burn(){
+    println!("burn token and gennrate burn proof");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+mod commands;
+
+#[derive(Debug, StructOpt)]
+enum Opt {
+    BurnAddress,
+    Burn,
+    Vote,
+    Verify
+}
+#[tokio::main]
+async fn main() {
+    let opt = Opt::from_args();
+    match opt {
+        Opt::BurnAddress => {
+            commands::burn_address::burn_address().await
+        }
+        Opt::Burn => {
+            commands::burn::burn().await
+        }
+        Opt::Vote => {
+            commands::vote::vote().await
+        }
+        Opt::Verify => {
+            commands::verify::verify().await
+        }
+    }
 }
