3a8fecd10a89bb3383cc0ff38624d4c2f61509eb
Pardis <pardis@Pardiss-MacBook-Air.local>
2024-12-17 10:15:54 +0100

    Feature(core): relocate burn address struct.

--- src/commands/burn_address.rs
+++ src/commands/burn_address.rs
@@ -1,22 +1,26 @@
 use alloy::{
     primitives::{keccak256, Address, U8},
-    signers::k256::SecretKey
+    hex::FromHex
 };
+use structopt::StructOpt;
 
 // 	BA = H(pkv  ||  vid || β  ||  pid || v)
 
-pub async fn burn_address(
-    private_key: SecretKey,
+#[derive(Debug, StructOpt)]
+pub struct BurnAddress {
+    private_key: String,
     ceremony_id: U8,
     blinding_factor: U8,
     personal_id: U8,
     vote: U8,
-) {
-    let private_key_bytes = private_key.to_bytes().into();
-    let personal_id_bytes_bytes = personal_id.to_be_bytes();
-    let blinding_factor_bytes = blinding_factor.to_be_bytes();
-    let ceremony_id_bytes = ceremony_id.to_be_bytes();
-    let vote_bytes = vote.to_be_bytes();
+}
+
+pub async fn burn_address(burn_address: BurnAddress) {
+    let private_key_bytes = <[u8; 32]>::from_hex(burn_address.private_key).unwrap();
+    let personal_id_bytes_bytes = burn_address.personal_id.to_be_bytes();
+    let blinding_factor_bytes = burn_address.blinding_factor.to_be_bytes();
+    let ceremony_id_bytes = burn_address.ceremony_id.to_be_bytes();
+    let vote_bytes = burn_address.vote.to_be_bytes();
 
     let data = [private_key_bytes, ceremony_id_bytes, blinding_factor_bytes, personal_id_bytes_bytes, vote_bytes].concat();
 
