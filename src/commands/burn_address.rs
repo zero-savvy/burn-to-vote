b8e66ec314b092c28f83e3e17c35fb1ccbaddd37
Pardis <pardis@Pardiss-MacBook-Air.local>
2024-12-17 10:07:24 +0100

    Feature(core): adding burn address loginc.

--- src/commands/burn_address.rs
+++ src/commands/burn_address.rs
@@ -1,3 +1,29 @@
-pub async fn burn_address(){
-    println!("generate burn address");
+use alloy::{
+    primitives::{keccak256, Address, U8},
+    signers::k256::SecretKey
+};
+
+// 	BA = H(pkv  ||  vid || β  ||  pid || v)
+
+pub async fn burn_address(
+    private_key: SecretKey,
+    ceremony_id: U8,
+    blinding_factor: U8,
+    personal_id: U8,
+    vote: U8,
+) {
+    let private_key_bytes = private_key.to_bytes().into();
+    let personal_id_bytes_bytes = personal_id.to_be_bytes();
+    let blinding_factor_bytes = blinding_factor.to_be_bytes();
+    let ceremony_id_bytes = ceremony_id.to_be_bytes();
+    let vote_bytes = vote.to_be_bytes();
+
+    let data = [private_key_bytes, ceremony_id_bytes, blinding_factor_bytes, personal_id_bytes_bytes, vote_bytes].concat();
+
+    let burn_address = Address::from_slice(
+        &keccak256(data).0[12..],
+    );
+
+    println!("Burn Address: {:?}", burn_address)
 }
+
