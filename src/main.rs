639b85933b371da2d9a6c42e8ec9d2e852fbdf25
Pardis <pardis@Pardiss-MacBook-Air.local>
2024-12-17 10:16:27 +0100

    Feature(core): clean up main.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use structopt::StructOpt;
 mod commands;
-use alloy::{
-    primitives::U8,
-    hex::FromHex,
-    signers::k256::SecretKey
-};
+use commands::burn_address::BurnAddress;
+
 #[derive(Debug, StructOpt)]
 enum Opt {
     BurnAddress(BurnAddress),
@@ -13,28 +10,12 @@ enum Opt {
     Verify,
 }
 
-#[derive(Debug, StructOpt)]
-struct BurnAddress {
-    private_key: String,
-    ceremony_id: U8,
-    blinding_factor: U8,
-    personal_id: U8,
-    vote: U8,
-}
 #[tokio::main]
 async fn main() {
     let opt = Opt::from_args();
     match opt {
-        Opt::BurnAddress(burn_address) => {
-            let key_bytes : [u8; 32] = <[u8; 32]>::from_hex(burn_address.private_key).unwrap();
-            let pk = SecretKey::from_bytes(&key_bytes.into()).unwrap();
-            commands::burn_address::burn_address(
-            pk,
-            burn_address.ceremony_id,
-            burn_address.blinding_factor,
-            burn_address.personal_id,
-            burn_address.vote,
-             ).await},
+        Opt::BurnAddress(burn_address) => 
+        commands::burn_address::burn_address(burn_address ).await,
         Opt::Burn => commands::burn::burn().await,
         Opt::Vote => commands::vote::vote().await,
         Opt::Verify => commands::verify::verify().await,
