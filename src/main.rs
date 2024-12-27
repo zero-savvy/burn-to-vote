c800119dacded492a6408d73e5bd244ce48aa4c0
pardis-toolabi <pardis.tlbi@gmail.com>
2024-12-27 03:08:29 +0100

    Feature(core): add burn struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use structopt::StructOpt;
 mod commands;
+use commands::burn::Burn;
 use commands::burn_address::BurnAddress;
 
 #[derive(Debug, StructOpt)]
 enum Opt {
     BurnAddress(BurnAddress),
-    Burn,
+    Burn(Burn),
     Vote,
     Verify,
 }
@@ -17,8 +18,8 @@ async fn main() {
         Opt::BurnAddress(burn_address) => {
             commands::burn_address::burn_address(burn_address).await;
         }
-        Opt::Burn => {
-            commands::burn::burn().await;
+        Opt::Burn(burn_data) => {
+            commands::burn::burn(burn_data).await;
         }
         Opt::Vote => {
             commands::vote::vote().await;
@@ -28,3 +29,4 @@ async fn main() {
         }
     }
 }
+// cargo run -- Burn --private-key <private_key> --ceremony-id <ceremony_id> --blinding-factor <blinding_factor> --personal-id <personal_id> --vote <vote>
