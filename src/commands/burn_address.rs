10ae48b64732d3e7a21e945d640a3b1184d1a2be
Pardis <prds.toolabi@gmail.com>
2025-01-07 11:34:46 +0100

    Feature(core): modify burn address command

--- src/commands/burn_address.rs
+++ src/commands/burn_address.rs
@@ -18,6 +18,7 @@ pub struct BurnAddress {
 pub async fn burn_address(burn_address: BurnAddress) -> Address {
     let blinding_factor = rand::random::<u64>();
     let private_key = U256::from_str_radix(&burn_address.private_key, 16).unwrap();
+
     let private_key_fp = u256_to_fp(private_key);
 
     let ceremony_id_fp = Fr::from_repr(FrRepr::from(burn_address.ceremony_id)).unwrap();
@@ -36,20 +37,20 @@ pub async fn burn_address(burn_address: BurnAddress) -> Address {
     let poseidon = Poseidon::new();
     let hash = poseidon.hash(input).unwrap();
     let rep = hash.into_repr();
+    let rep_str = hash.into_repr().to_string();
     let bytes = fr_repr_to_bytes(&rep);
     let address_bytes = &bytes[12..];
     let address = Address::from_slice(address_bytes);
-    println!("burn_address: 0x{}", hex::encode(address_bytes));
 
     let inputs_json = format!(
-        "{{ \"burn_address\": {},
-        \"private_key\": {},
-        \"blinding_factor\": {},
-        \"ceremony_id\": {},
-        \"personal_id\": {},
-        \"vote\": {} }}",
-        serde_json::to_string(&address).unwrap(),
-        serde_json::to_string(&burn_address.private_key).unwrap(),
+        "{{ \"address\": \"{}\",
+        \"privateKey\": \"{}\",
+        \"blinding_factor\": \"{}\",
+        \"ceremonyID\": \"{}\",
+        \"personalID\": \"{}\",
+        \"vote\": \"{}\" }}",
+        U256::from_str_radix(&rep_str[2..], 16).unwrap().to_string(),
+        private_key.to_string(),
         serde_json::to_string(&blinding_factor).unwrap(),
         serde_json::to_string(&burn_address.ceremony_id).unwrap(),
         serde_json::to_string(&burn_address.personal_id).unwrap(),
