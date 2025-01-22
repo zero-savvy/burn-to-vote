187bafa6806d3189493ed6de6716881fb272e493
pardis-toolabi <pardis.tlbi@gmail.com>
2025-01-22 13:28:42 +0100

    Feature(core): add nullifier format_inputs.

--- src/circuits/nullifier.rs
+++ src/circuits/nullifier.rs
@@ -1,10 +1,9 @@
-use std::fs::{self, File};
 use primitive_types::U256;
-use std::io::Write;
-use log::{info, error};
-use crate::circuits::Circuit;
+use crate::circuits::{Circuit, CircuitIdentifier};
+
 
 pub struct NullifierCircuit {
+    identifier: CircuitIdentifier<'static>,
     private_key: U256,
     blinding_factor: u64,
     ceremony_id: u64,
@@ -14,17 +13,14 @@ pub struct NullifierCircuit {
 impl NullifierCircuit {
     pub fn new(private_key: U256, blinding_factor: u64, ceremony_id: u64, nullifier: String) -> Self {
         Self {
+            identifier: CircuitIdentifier { circuit_name: "nullifier" },
             private_key,
             blinding_factor,
             ceremony_id,
             nullifier,
         }
     }
-}
-
-impl Circuit for NullifierCircuit {
-    fn generate_inputs(&self) -> Result<(), Box<dyn std::error::Error>> {
-        info!("Generating inputs ...");
+    pub fn format_inputs(&self) -> Result<String, Box<dyn std::error::Error>> {
         let inputs = format!(
             "{{\"privateKey\": \"{}\",
             \"blindingFactor\": \"{}\",
@@ -35,116 +31,39 @@ impl Circuit for NullifierCircuit {
             self.ceremony_id.to_string(),
             U256::from_str_radix(&self.nullifier[2..], 16).unwrap().to_string()
         );
+        Ok(inputs)
+    }
+}
 
-        let inputs_path = "inputs/nullifier.json";
-        let mut file = File::create(inputs_path)?;
-        file.write_all(inputs.to_string().as_bytes())?;
-        info!("Input generated.");
+impl Circuit for NullifierCircuit {
+    fn generate_input_file(&self, inputs:String) -> Result<(), Box<dyn std::error::Error>> {
+        self.identifier.generate_input_file(inputs)?;
         Ok(())
     }
 
     fn generate_witness(&self) -> Result<(), Box<dyn std::error::Error>> {
-        info!("Generating witness ...");
-        let calculate_command = "snarkjs wtns calculate circuits/nullifier/nullifier_js/nullifier.wasm inputs/nullifier.json circuits/nullifier/witness.wtns";
-        let calculate_output = std::process::Command::new("sh")
-            .arg("-c")
-            .arg(&calculate_command)
-            .output()?;
-
-        if !calculate_output.status.success() {
-            let calculate_stdout = String::from_utf8_lossy(&calculate_output.stdout);
-            println!("Standard output:\n{}", calculate_stdout);
-            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed to generate witness")));
-        }
-
-        let export_command = "snarkjs wtns export json circuits/nullifier/witness.wtns circuits/nullifier/witness.json";
-        let export_output = std::process::Command::new("sh")
-            .arg("-c")
-            .arg(&export_command)
-            .output()?;
-
-        if !export_output.status.success() {
-            let export_stdout = String::from_utf8_lossy(&export_output.stdout);
-            println!("Standard output:\n{}", export_stdout);
-            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed to export witness")));
-        }
-
-        info!("Witness Generated.");
+        self.identifier.generate_witness()?;
         Ok(())
     }
 
     fn setup_zkey(&self) -> Result<(), Box<dyn std::error::Error>> {
-        info!("Setting up zkey ...");
-        let setup_command = "snarkjs groth16 setup circuits/nullifier/nullifier.r1cs circuits/setup/pot12_final.ptau circuits/nullifier/nullifier_0000.zkey";
-        let output = std::process::Command::new("sh")
-            .arg("-c")
-            .arg(setup_command)
-            .output()?;
-
-        if !output.status.success() {
-            let stdout = String::from_utf8_lossy(&output.stdout);
-            println!("Standard output:\n{}", stdout);
-            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed to setup zkey")));
-        }
-
-        info!("Zkey Generated.");
+        self.identifier.setup_zkey()?;
         Ok(())
     }
 
     fn generate_proof(&self) -> Result<(), Box<dyn std::error::Error>> {
-        info!("Generating proof ...");
-        fs::create_dir_all("circuits/proofs")?;
-
-        let command = "snarkjs groth16 prove circuits/nullifier/nullifier_0000.zkey circuits/nullifier/witness.wtns circuits/proofs/nullifier_proof.json circuits/proofs/nullifier_public.json";
-        let output = std::process::Command::new("sh")
-            .arg("-c")
-            .arg(command)
-            .output()?;
-
-        if !output.status.success() {
-            let stdout = String::from_utf8_lossy(&output.stdout);
-            println!("Standard output:\n{}", stdout);
-            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed to generate proof")));
-        }
-
-        info!("Proof Generated.");
+        self.identifier.generate_proof()?;
 
         Ok(())
     }
 
     fn setup_vkey(&self) -> Result<(), Box<dyn std::error::Error>> {
-        info!("Setting up vkey ...");
-        let setup_command = "snarkjs zkey export verificationkey circuits/nullifier/nullifier_0000.zkey circuits/nullifier/verification_key.json";
-        let output = std::process::Command::new("sh")
-            .arg("-c")
-            .arg(&setup_command)
-            .output()?;
-
-        if !output.status.success() {
-            let stdout = String::from_utf8_lossy(&output.stdout);
-            println!("Standard output:\n{}", stdout);
-            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed to setup vkey")));
-        }
-
-        info!("Vkey Generated.");
+        self.identifier.setup_vkey()?;
         Ok(())
     }
     
     fn verify_proof(&self) -> Result<(), Box<dyn std::error::Error>> {
-        info!("Verifying proof ...");
-        let command = "snarkjs groth16 verify circuits/nullifier/verification_key.json circuits/proofs/nullifier_public.json circuits/proofs/nullifier_proof.json";
-        let output = std::process::Command::new("sh")
-            .arg("-c")
-            .arg(command)
-            .output()?;
-
-        if !output.status.success() {
-            let stdout = String::from_utf8_lossy(&output.stdout);
-            println!("Standard output:\n{}", stdout);
-            return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Failed to verify proof")));
-        }
-
-        info!("PROOF VERIFIED.");
+        self.identifier.verify_proof()?;
         Ok(())
     }
 }
