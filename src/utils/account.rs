3b17bdee23ff4e8f61780c1cb9a14aeac3f41a83
pardis-toolabi <pardis.tlbi@gmail.com>
2025-04-29 18:59:06 +0200

    add leaf nibbles

--- src/utils/account.rs
+++ src/utils/account.rs
@@ -67,17 +67,7 @@ pub async fn prepare_mpt_data(burn_address: Address, provider: Provider<Http>) -
         decoded_proof_bytes.push(rlp_decode.clone());
 
         let mut node_hex_array = serialize_hex(&hex::encode(item));
-        log::info!("item{:?}", item.to_vec());
-        log::info!("len:   {:?}", item.to_vec().len());
-        log::info!("node_hex_array{:?}", node_hex_array);
-        log::info!("len:  {:?}", node_hex_array.len());
-        log::info!("decoded nodes: {:?}",rlp_decode.clone());
-        log::info!("decoded len: {:?}",rlp_decode.len());
-        let hex_hash = hex::encode(keccak256(item.clone().to_vec()));
-        let ssss = serialize_hex(&hex_hash);
-        log::info!("node hash: {:?}", ssss);
-        log::info!("//////////////////////////////////////////////////////////////////////////");
-
+  
         let len = node_hex_array.len();
 
         prooflen.push(len);
@@ -88,30 +78,28 @@ pub async fn prepare_mpt_data(burn_address: Address, provider: Provider<Http>) -
         proof.push(node_hex_array); 
     }
 
-    let node_types = get_mpt_node_type(decoded_proof_bytes.clone());
+    let mut node_types = get_mpt_node_type(decoded_proof_bytes.clone());
     log::info!("prooflen{:?}",node_types.len());
     log::info!("prooflen{:?}",node_types);
     let address_hash =hex::encode(keccak256(burn_address));
     let sa = serialize_hex(&address_hash);
     log::info!("serialize address: {:?}",sa);
+    log::info!("burn_address: {:?}",burn_address);
     log::info!("decoded nodes: {:?}",decoded_proof_bytes.len());
-    let mut decoded_noded_length: Vec<Vec<usize>> = Vec::new();
+    let mut node_items_len: Vec<Vec<usize>> = Vec::new();
     let mut node_lengths = Vec::new();
-    for item in decoded_proof_bytes{
+    for item in decoded_proof_bytes.clone(){
         let mut node_length = Vec::new();
         node_lengths.push(item.len());
         for i in item.clone() {
             node_length.push(i.len());
         }
         let nl: Vec<usize> = node_length.iter().map(|x| (x + 1)*2).collect();
-        decoded_noded_length.push(nl);
+        node_items_len.push(nl);
         log::info!("decoded nodes: {:?}",item);
         log::info!("decoded nodes: {:?}",item.len());
     }
     log::info!("address bytes: {:?}",keccak256(burn_address).to_vec());
-    log::info!("decoded_noded_length: {:?}",decoded_noded_length);
-    log::info!("decoded_noded_length: {:?}",node_lengths);
-    // log::info!("decoded nodes: {:?}",decoded_proof_bytes[1]);
 
     let aaaaaa = hex::encode([51, 93, 253, 141, 184, 39, 12, 245, 120, 220, 164, 70, 79, 239, 185, 39, 116, 83, 11, 202, 65, 208, 249, 226, 254, 161, 123, 220, 4, 202, 124, 214]);
     let h = serialize_hex(&aaaaaa);
@@ -126,9 +114,46 @@ pub async fn prepare_mpt_data(burn_address: Address, provider: Provider<Http>) -
         prooflen.push(0);
     }
 
+
+    for item in &mut node_items_len {
+        if item.len() < 17 {
+            let diff = 17 - item.len();
+            item.resize(item.len() + diff, 0);
+        }
+    }
+
+
+
+    log::info!("node_items_len: {:?}", node_items_len);
+
+    // get extension nibbles
+    let mut extension_nodes_shared_nibbles = [0;8];
+    for (i, item) in node_types.iter().enumerate() {
+        if *item == 0{
+            let nibs = &decoded_proof_bytes[i][0];
+            // bytes
+            let nibs_len = nibs.len();
+            // -1 is to remove the prefix of leaf and extension nodes
+            let nibs_len_without_prefix = (nibs_len - 1) * 2;
+            extension_nodes_shared_nibbles[i] = nibs_len_without_prefix;
+        }
+    }
+
+    let leaf_nibbles = extension_nodes_shared_nibbles[real_proof_len -1 ];
+
+    let ba = hex::encode(burn_address.clone());
+    let s_ba: Vec<u8> = serialize_hex(&ba);
+
+
+    
+
+
+
+
     info!("MPT circuit data generated.");
 
     MptCircuit::new(
+        s_ba,
         addres_proof.nonce,
         addres_proof.balance,
         addres_proof.code_hash.to_fixed_bytes(),
@@ -139,19 +164,8 @@ pub async fn prepare_mpt_data(burn_address: Address, provider: Provider<Http>) -
         proof.clone(),
         real_proof_len,
         prooflen,
+        leaf_nibbles
+
     )
     
 }
-
-
-//  (len -1 )node hex len is 358
-// [32, 32, 0, 0, 0, 0, 0, 0, 0, 0, 32, 32, 0, 32, 0, 0, 0]
-// 12 item 0 
-// [32, 0, 0, 0, 0, 32, 0, 32, 32, 32,
-//  0, 0, 32, 0, 0, 0, 0]
-
-
-// node_hex_array[15, 8, 13, 1, 10, 0, 6, 13, 10, 2, 0, 6, 14, 7, 15, 10, 2, 1, 4, 4, 11, 5, 5, 8, 2, 7, 2, 2, 0, 13, 8, 7, 11, 1, 15, 13, 3, 5, 12, 7, 10, 15, 5, 10, 13, 2, 5, 2, 5, 2, 1, 14, 11, 13, 7, 6, 8, 6, 8, 8, 3, 10, 11, 3, 0, 6, 6, 10, 3, 2, 8, 0, 8, 0, 8, 0, 8, 0, 10, 0, 6, 14, 1, 13, 15, 7, 14, 7, 0, 12, 2, 9, 8, 12, 2, 7, 4, 14, 10, 3, 1, 5, 12, 0, 12, 4, 12, 14, 14, 6, 6, 2, 5, 2, 13, 1, 3, 15, 10, 12, 4, 13, 6, 10, 4, 15, 3, 1, 11, 7, 12, 5, 14, 0, 6, 15, 1, 4, 6, 0, 2, 8, 0, 13, 8, 0, 10, 0, 14, 1, 1, 13, 11, 10, 14, 4, 1, 2, 0, 2, 0, 14, 5, 2, 1, 5, 7, 3, 11, 6, 11, 9, 3, 5, 4, 7, 4, 1, 14, 11, 15, 1, 14, 1, 7, 1, 14, 3, 2, 14, 8, 5, 8, 15, 7, 3, 4, 15, 3, 15, 13, 13, 1, 2, 14, 3, 13, 15, 3, 1, 11, 13, 10, 0, 2, 14, 11, 0, 9, 15, 15, 0, 3, 15, 5, 15, 2, 5, 2, 0, 6, 10, 6, 1, 0, 11, 6, 7, 0, 8, 10, 12, 8, 11, 5, 14, 15, 15, 8, 12, 8, 5, 7, 2, 10, 12, 7, 15, 14, 0, 3, 15, 1, 15, 1, 7, 15, 5, 14, 15, 6, 7, 14, 5, 6, 10, 2, 1, 10, 0, 3, 3, 5, 13, 15, 13, 8, 13, 11, 8, 2, 7, 0, 12, 15, 5, 7, 8, 13, 12, 10, 4, 4, 6, 4, 15, 14, 15, 11, 9, 2, 7, 7, 4, 5, 3, 0, 11, 12, 10, 4, 1, 13, 0, 15, 9, 14, 2, 15, 14, 10, 1, 7, 11, 13, 12, 0, 4, 12, 10, 7, 12, 13, 6,
-//  8, 0, 8, 0, 10, 0, 13, 14, 14, 10, 15, 5, 7, 6, 3, 9, 6, 1, 3, 11, 0, 9, 1, 6, 6, 8, 12, 3, 4, 4, 1, 4, 12, 13, 5, 10, 6, 4, 10, 14, 4, 8, 14, 5, 15, 12, 3, 0, 3, 6, 14, 12, 15, 12, 5, 8, 1, 8, 6, 5, 9, 1, 15, 14, 6, 3, 0, 5, 6, 12, 8, 0, 8, 0, 8, 0, 8, 0]
-
-// [32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 0],
