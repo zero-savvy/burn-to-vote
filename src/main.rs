eb390509d4cc59a6dff5a446a10befdbb1aabd9d
pardis-toolabi <pardis.tlbi@gmail.com>
2025-05-01 17:00:39 +0200

    add provider to main

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ use structopt::StructOpt;
 mod circuits;
 mod commands;
 mod utils;
+use alloy::primitives::{address, Address};
 use commands::burn::Burn;
 use commands::burn_address::BurnAddress;
+use commands::demo::DemoData;
 use commands::merkle_tree::UserIndex;
 use commands::nullifier::Nullifier;
 use commands::vote::Vote;
 use env_logger::Env;
+use ethers::providers::{Http, Provider};
+use std::fs::{self, File};
+use std::path::Path;
 
 // TO DO:
 // error handling
@@ -28,11 +33,19 @@ enum Opt {
     Verify,
     GenerateTree,
     GenerateProof(UserIndex),
+    Demo(DemoData),
 }
 
 #[tokio::main]
 async fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let provider: Provider<Http> = Provider::<Http>::try_from("http://localhost:8545/")
+        .unwrap()
+        .clone();
+
+    let path = Path::new("data/whitelist.json");
+    let file = File::open(path).unwrap();
+    let mut whitelist: Vec<Address> = serde_json::from_reader(file).unwrap();
 
     let opt = Opt::from_args();
     match opt {
@@ -40,23 +53,26 @@ async fn main() {
             commands::burn_address::burn_address(burn_address).await;
         }
         Opt::Burn(burn_data) => {
-            commands::burn::burn(burn_data).await;
+            commands::burn::burn(burn_data, provider).await;
         }
         Opt::Nullifier(nullifier) => {
             commands::nullifier::generate_nullifier(nullifier);
         }
         Opt::Vote(vote_data) => {
-            commands::vote::vote(vote_data).await;
+            commands::vote::vote(vote_data, provider).await;
         }
         Opt::GenerateTree => {
-            commands::merkle_tree::generate_tree().await;
+            commands::merkle_tree::generate_tree(&mut whitelist).await;
         }
         Opt::GenerateProof(user_index) => {
-            let tree = commands::merkle_tree::generate_tree().await;
-            commands::merkle_tree::generate_proof(tree, user_index.index).await;
+            let tree = commands::merkle_tree::generate_tree(&mut whitelist).await;
+            commands::merkle_tree::generate_proof(&tree, user_index.index).await;
         }
         Opt::Verify => {
             commands::verify::verify().await;
+        }
+        Opt::Demo(demo_data) => {
+            commands::demo::demo(demo_data).await;
         }
     }
 }
