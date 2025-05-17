bbecb309859488b7c1acfb076d0fd774f2515959
pardis-toolabi <pardis.tlbi@gmail.com>
2025-05-17 10:39:15 +0200

    update test deploy

--- contracts/script/Voting.s.sol
+++ contracts/script/Voting.s.sol
@@ -18,17 +18,16 @@ contract VotingScript is Script {
         // uint256 base_time = 1000;
         // vm.warp(base_time);
 
-
         uint256 voting_time = block.timestamp + 10000;
         uint256 tally_time = block.timestamp + 20000;
+        uint256 ceremeny_id = 14564730128827983570;
+        uint256 mt = 16140668641613092893634150840665751551734694739321141751642452107309003732465;
 
         verifier = new Groth16Verifier();
-        voting = new Voting(address(verifier),voting_time,tally_time);
+        voting = new Voting(address(verifier), voting_time, tally_time, mt, ceremeny_id);
 
         vm.stopBroadcast();
         console.logAddress(address(voting));
-
-
     }
 }
 // forge script VotingScript --rpc-url http://localhost:8545 --broadcast
