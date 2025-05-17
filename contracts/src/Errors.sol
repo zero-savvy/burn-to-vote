4bf46c219548c93dbaaa691db79974f6d08b1ed1
pardis-toolabi <pardis.tlbi@gmail.com>
2025-05-17 10:39:42 +0200

    add errors contract

---
+++ contracts/src/Errors.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+error VotingPeriodEnded(uint256 voteSubmissionDeadline, uint256 request_time);
+error InvalidVote(uint256 voteValue);
+error NullifierAlreadyUsed(uint256 nullifier);
+error RevotingNotAllowed();
+error InvalidMerkleRoot(uint256 provided, uint256 expected);
+error InvalidCeremonyId(uint256 provided, uint256 expected);
+error InvalidProof();
+error InvalidRevoteValue();
+error NullifierMismatch(uint256 voteNullifier, uint256 revoteNullifier);
