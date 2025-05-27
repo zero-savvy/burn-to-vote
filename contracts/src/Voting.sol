6ac419620758983a95e5a9269b8023405b852797
pardis-toolabi <pardis.tlbi@gmail.com>
2025-05-27 20:14:46 +0200

    adding state root

--- contracts/src/Voting.sol
+++ contracts/src/Voting.sol
@@ -11,6 +11,7 @@ contract Voting {
     uint256 public merkle_root;
     uint256 public voteSubmissionDeadline;
     uint256 public tallyDeadline;
+    uint256 public state_root;
 
     uint128 private yesVotes;
     uint128 private noVotes;
@@ -28,7 +29,8 @@ contract Voting {
         uint256 _submissionDeadline,
         uint256 _tallyDeadline,
         uint256 _merkle_root,
-        uint256 _ceremony_id
+        uint256 _ceremony_id,
+        uint256 _state_root
     ) {
         require(_submissionDeadline < _tallyDeadline, "Submission deadline must be before tally deadline");
         verifier = Groth16Verifier(_verifier);
@@ -36,76 +38,89 @@ contract Voting {
         tallyDeadline = _tallyDeadline;
         merkle_root = _merkle_root;
         ceremony_id = _ceremony_id;
+        state_root =_state_root;
     }
 
     function submitVote(
         uint256[2] calldata proofA,
         uint256[2][2] calldata proofB,
         uint256[2] calldata proofC,
-        uint256[5] calldata pubSignals
+        uint256[6] calldata pubSignals
     ) external {
         if (block.timestamp > voteSubmissionDeadline) revert VotingPeriodEnded(voteSubmissionDeadline, block.timestamp);
-        if (!(pubSignals[2] == 0 || pubSignals[2] == 1)) revert InvalidVote(pubSignals[2]);
-        if (usedNullifiers[pubSignals[0]] != 0) revert NullifierAlreadyUsed(pubSignals[0]);
-        if (pubSignals[3] == 1) revert RevotingNotAllowed();
-        if (pubSignals[4] != merkle_root) revert InvalidMerkleRoot(pubSignals[4], merkle_root);
-        if (pubSignals[1] != ceremony_id) revert InvalidCeremonyId(pubSignals[1], ceremony_id);
+        if (!(pubSignals[3] == 0 || pubSignals[3] == 1)) revert InvalidVote(pubSignals[2]);
+        if (usedNullifiers[pubSignals[1]] != 0) revert NullifierAlreadyUsed(pubSignals[0]);
+        if (pubSignals[4] == 1) revert RevotingNotAllowed();
+        if (pubSignals[5] != merkle_root) revert InvalidMerkleRoot(pubSignals[5], merkle_root);
+        if (pubSignals[2] != ceremony_id) revert InvalidCeremonyId(pubSignals[2], ceremony_id);
+        if (pubSignals[0] != state_root) revert InvalidStateroot(pubSignals[0], state_root);
 
         bool proofIsValid = verifier.verifyProof(proofA, proofB, proofC, pubSignals);
         if (!proofIsValid) revert InvalidProof();
 
-        usedNullifiers[pubSignals[0]] += 1;
+        usedNullifiers[pubSignals[1]] += 1;
 
-        if (pubSignals[2] == 1) {
+        if (pubSignals[3] == 1) {
             yesVotes++;
         } else {
             noVotes++;
         }
 
-        emit VoteSubmitted(msg.sender, pubSignals[0], pubSignals[2]);
+        emit VoteSubmitted(msg.sender, pubSignals[1], pubSignals[3]);
     }
+
+    
 
     function submitRevote(
         uint256[2] calldata old_proofA,
         uint256[2][2] calldata old_proofB,
         uint256[2] calldata old_proofC,
-        uint256[5] calldata old_pubSignals,
+        uint256[6] calldata old_pubSignals,
         uint256[2] calldata new_proofA,
         uint256[2][2] calldata new_proofB,
         uint256[2] calldata new_proofC,
-        uint256[5] calldata new_pubSignals
+        uint256[6] calldata new_pubSignals
     ) external {
         if (block.timestamp > voteSubmissionDeadline) revert VotingPeriodEnded(voteSubmissionDeadline, block.timestamp);
 
-        if (old_pubSignals[0] != new_pubSignals[0]) revert NullifierMismatch(old_pubSignals[0], new_pubSignals[0]);
+        if (old_pubSignals[1] != new_pubSignals[1]) revert NullifierMismatch(old_pubSignals[1], new_pubSignals[1]);
 
-        if (!(old_pubSignals[2] == 0 || old_pubSignals[2] == 1)) revert InvalidVote(old_pubSignals[2]);
-        if (old_pubSignals[3] == 1) revert RevotingNotAllowed();
-        if (old_pubSignals[4] != merkle_root) revert InvalidMerkleRoot(old_pubSignals[4], merkle_root);
-        if (old_pubSignals[1] != ceremony_id) revert InvalidCeremonyId(old_pubSignals[1], ceremony_id);
+        if (!(old_pubSignals[3] == 0 || old_pubSignals[3] == 1)) revert InvalidVote(old_pubSignals[3]);
+        if (old_pubSignals[4] == 1) revert RevotingNotAllowed();
+        if (old_pubSignals[5] != merkle_root) revert InvalidMerkleRoot(old_pubSignals[5], merkle_root);
+        if (old_pubSignals[2] != ceremony_id) revert InvalidCeremonyId(old_pubSignals[2], ceremony_id);
+        if (old_pubSignals[0] != state_root) revert InvalidStateroot(old_pubSignals[0], state_root);
         bool oldProofIsValid = verifier.verifyProof(old_proofA, old_proofB, old_proofC, old_pubSignals);
-        if (!oldProofIsValid) revert InvalidProof();
 
-        if (!(new_pubSignals[2] == 0 || new_pubSignals[2] == 1)) revert InvalidVote(new_pubSignals[2]);
-        if (new_pubSignals[2] == old_pubSignals[2]) revert InvalidRevoteValue();
-        if (usedNullifiers[new_pubSignals[0]] != 1) revert NullifierAlreadyUsed(new_pubSignals[0]);
-        if (new_pubSignals[3] == 0) revert RevotingNotAllowed();
-        if (new_pubSignals[4] != merkle_root) revert InvalidMerkleRoot(new_pubSignals[4], merkle_root);
-        if (new_pubSignals[1] != ceremony_id) revert InvalidCeremonyId(new_pubSignals[1], ceremony_id);
+
+        if (!oldProofIsValid) revert InvalidProof();
+        
+
+        if (!(new_pubSignals[3] == 0 || new_pubSignals[3] == 1)) revert InvalidVote(new_pubSignals[3]);
+        if (new_pubSignals[3] == old_pubSignals[3]) revert InvalidRevoteValue();
+        if (usedNullifiers[new_pubSignals[1]] != 1) revert NullifierAlreadyUsed(new_pubSignals[0]);
+        if (new_pubSignals[4] == 0) revert RevotingNotAllowed();
+        if (new_pubSignals[5] != merkle_root) revert InvalidMerkleRoot(new_pubSignals[5], merkle_root);
+        if (new_pubSignals[5] != merkle_root) revert InvalidMerkleRoot(new_pubSignals[5], merkle_root);
+        if (new_pubSignals[5] != merkle_root) revert InvalidMerkleRoot(new_pubSignals[5], merkle_root);
+        if (new_pubSignals[5] != merkle_root) revert InvalidMerkleRoot(new_pubSignals[5], merkle_root);
+        if (new_pubSignals[5] != merkle_root) revert InvalidMerkleRoot(new_pubSignals[5], merkle_root);
+        if (new_pubSignals[2] != ceremony_id) revert InvalidCeremonyId(new_pubSignals[2], ceremony_id);
+        if (new_pubSignals[0] != state_root) revert InvalidStateroot(new_pubSignals[0], state_root);
         bool proofIsValid = verifier.verifyProof(new_proofA, new_proofB, new_proofC, new_pubSignals);
         if (!proofIsValid) revert InvalidProof();
 
-        usedNullifiers[old_pubSignals[2]] += 1;
+        usedNullifiers[old_pubSignals[3]] += 1;
 
-        if (old_pubSignals[3] == 1 && new_pubSignals[3] == 0) {
+        if (old_pubSignals[4] == 1 && new_pubSignals[4] == 0) {
             yesVotes--;
             noVotes++;
-        } else if (old_pubSignals[3] == 0 && new_pubSignals[3] == 1) {
+        } else if (old_pubSignals[4] == 0 && new_pubSignals[4] == 1) {
             noVotes--;
             yesVotes++;
         }
 
-        emit VoteSubmitted(msg.sender, new_pubSignals[2], new_pubSignals[3]);
+        emit VoteSubmitted(msg.sender, new_pubSignals[3], new_pubSignals[4]);
     }
 
     function tallyVotes() external {
@@ -124,6 +139,3 @@ contract Voting {
     }
 }
 
-// proving time
-// gas estimatuin
-// compare to others in large scale
