6f8df2c76012c707fabf0d6a6d96a6849a61f958
pardis-toolabi <pardis.tlbi@gmail.com>
2025-06-03 07:24:20 +0200

    refactor nullifier command

--- src/commands/nullifier.rs
+++ src/commands/nullifier.rs
@@ -1,5 +1,5 @@
-use crate::circuits::nullifier_c::*;
-use crate::circuits::Circuit;
+
+use crate::utils::config::*;
 use crate::utils::u256_to_fp;
 use ff::PrimeField;
 use log::info;
@@ -7,19 +7,21 @@ use poseidon_rs::{Fr, FrRepr, Poseidon};
 use primitive_types::U256;
 use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, StructOpt, Clone)]
 pub struct Nullifier {
     pub private_key: String,
     pub ceremony_id: u64,
     pub blinding_factor: u64,
 }
 
-pub fn generate_nullifier(data: Nullifier) -> String {
-    let private_key = U256::from_str_radix(&data.private_key, 16).unwrap();
+pub fn generate_nullifier(data: Config, blinding_factor: u64, private_key: String) -> String {
+    info!("generaring nullifier ..");
+
+    let private_key = U256::from_str_radix(&private_key, 16).unwrap();
     let private_key_fp = u256_to_fp(private_key);
 
-    let blinding_factor_fp = Fr::from_repr(FrRepr::from(data.blinding_factor)).unwrap();
-    let ceremony_id_fp = Fr::from_repr(FrRepr::from(data.ceremony_id)).unwrap();
+    let blinding_factor_fp = Fr::from_repr(FrRepr::from(blinding_factor)).unwrap();
+    let ceremony_id_fp = Fr::from_repr(FrRepr::from(data.ceremony_id.unwrap())).unwrap();
 
     let input: Vec<Fr> = vec![private_key_fp, ceremony_id_fp, blinding_factor_fp];
 
