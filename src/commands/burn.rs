1a248f7b6562ea201eb6b6d85d192f05f06d24a5
pardis-toolabi <pardis.tlbi@gmail.com>
2025-06-03 07:26:51 +0200

    refactor burn command

--- src/commands/burn.rs
+++ src/commands/burn.rs
@@ -1,37 +1,33 @@
-use crate::circuits::Circuit;
-use crate::utils::account::prepare_mpt_data;
-use crate::{
-    circuits::mpt_c::MptCircuit,
-    utils::{
-        account::{get_account_proof, get_account_rlp},
-        get_mpt_node_type, hexToBytes, serialize_hex,
-    },
-};
 use ethers::{
     core::types::TransactionRequest,
     middleware::SignerMiddleware,
     prelude::*,
     providers::{Http, Middleware, Provider},
     signers::{LocalWallet, Signer},
-    utils::{self, keccak256, rlp},
+    utils::parse_ether,
 };
-use log::{info, log};
-use serde::ser::SerializeTuple;
-
-use hex;
-use std::{clone, convert::TryFrom};
+use log::info;
 use structopt::StructOpt;
 type PrimitiveU256 = primitive_types::U256;
-use super::super::utils::account;
 
-#[derive(Debug, StructOpt)]
+#[derive(Debug, StructOpt, Clone)]
 pub struct Burn {
     pub private_key: String,
     pub burn_address: Address,
     pub amount: PrimitiveU256,
 }
 
-pub async fn burn(burn_data: Burn, provider: Provider<Http>) -> (H256, Provider<Http>) {
+pub async fn burn(
+    provider: Provider<Http>,
+    burn_address: Address,
+    amount: PrimitiveU256,
+    private_key: String,
+) -> (H256, Provider<Http>) {
+    let burn_data = Burn {
+        private_key,
+        burn_address,
+        amount,
+    };
     info!("Burnig {:?} Eth ...", burn_data.amount);
 
     let chain_id = provider.get_chainid().await.unwrap();
@@ -43,17 +39,22 @@ pub async fn burn(burn_data: Burn, provider: Provider<Http>) -> (H256, Provider<
 
     let to_address = burn_data.burn_address;
 
-    let client = SignerMiddleware::new(provider.clone(), wallet);
+    let client = SignerMiddleware::new(provider.clone(), wallet.clone());
 
     let pre_tx_balance = provider
         .clone()
         .get_balance(burn_data.burn_address, None)
         .await
         .unwrap();
+    let raw_wei = parse_ether("0.0001").unwrap(); // U256
+    let scaled = raw_wei / U256::exp10(15);
+
+    let balance: U256 = provider.get_balance(wallet.address(), None).await.unwrap();
+    info!("balance{:?}", balance);
 
     let tx = TransactionRequest::new()
         .to(to_address)
-        .value(U256::from(utils::parse_ether(burn_data.amount).unwrap()));
+        .value(U256::from(150));
 
     let pending_tx = client.send_transaction(tx, None).await.unwrap();
     let receipt = pending_tx
@@ -61,7 +62,7 @@ pub async fn burn(burn_data: Burn, provider: Provider<Http>) -> (H256, Provider<
         .unwrap()
         .ok_or_else(|| eyre::format_err!("tx dropped from mempool"))
         .unwrap();
-    let tx = client
+    let _tx = client
         .get_transaction(receipt.transaction_hash)
         .await
         .unwrap();
@@ -73,9 +74,9 @@ pub async fn burn(burn_data: Burn, provider: Provider<Http>) -> (H256, Provider<
         .unwrap();
 
     assert_eq!(
-        pre_tx_balance + U256::from(utils::parse_ether(burn_data.amount).unwrap()),
-        post_tx_balance
-    );
+            pre_tx_balance + U256::from(parse_ether(burn_data.amount).unwrap()),
+            post_tx_balance
+        );
     info!("Burn transaction hash: {:?}", receipt.transaction_hash);
 
     (receipt.transaction_hash, provider)
