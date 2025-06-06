67d660ac5541190cb8515ef1b5e47b13dc375a0a
pardis-toolabi <pardis.tlbi@gmail.com>
2025-06-06 15:46:06 +0200

    clean main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod commands;
 mod db;
 mod utils;
 use db::VotingDB;
+use log::info;
 use std::{path::Path, sync::Arc};
 use structopt::StructOpt;
 use utils::config::Opt;
@@ -23,26 +24,52 @@ async fn main() -> anyhow::Result<()> {
             cfg.initiate_ceremony().await;
             db.ceremonies().save_ceremony(cfg);
         }
-        other => {
-            let mut cfg = db.ceremonies.get_ceremony(None)?;
-
-            let res = match other {
-                Opt::Vote(v) => commands::vote::vote(&mut cfg, v).await,
-                Opt::Tally => commands::tally::tally(&mut cfg).await,
-                Opt::Demo(d) => commands::demo::demo(&mut cfg, d).await,
-                Opt::OnchainDemo(d) => commands::onchain_demo::onchain_demo(&mut cfg, d).await,
-                _ => unreachable!(),
-            };
-
-            match res {
-                Ok(()) => {
-                    db.ceremonies().save_ceremony(cfg)?;
-                }
-                Err(e) => {
-                    eprintln!("Error: {:?}", e);
-                }
+        Opt::Vote(v) => {
+            let mut cfg;
+            if let Some(id) = v.ceremony_id {
+                cfg = db.ceremonies.get_ceremony(Some(id))?;
+            } else {
+                cfg = db.ceremonies.get_ceremony(None)?;
             }
+            commands::vote::vote(&mut cfg, v).await;
+            db.ceremonies().save_ceremony(cfg)?;
         }
+        Opt::Tally(t) => {
+            let mut cfg;
+            if let Some(id) = t.ceremony_id {
+                cfg = db.ceremonies.get_ceremony(Some(id))?;
+            } else {
+                cfg = db.ceremonies.get_ceremony(None)?;
+            }
+            commands::tally::tally(&mut cfg).await;
+            db.ceremonies().save_ceremony(cfg)?;
+        }
+        Opt::ListCeremonies =>{
+            let list  =db.ceremonies().get_all_ceremonies()?;
+            info!("all ceremonies: {:?}", list)
+        }
+        Opt::Demo(d) => {
+            let mut cfg;
+            if let Some(id) = d.ceremony_id {
+                cfg = db.ceremonies.get_ceremony(Some(id))?;
+            } else {
+                cfg = db.ceremonies.get_ceremony(None)?;
+            }
+            commands::demo::demo(&mut cfg, d).await;
+            db.ceremonies().save_ceremony(cfg)?;
+        }
+        Opt::OnchainDemo(d) => {
+            let mut cfg;
+            if let Some(id) = d.ceremony_id {
+                cfg = db.ceremonies.get_ceremony(Some(id))?;
+            } else {
+                cfg = db.ceremonies.get_ceremony(None)?;
+            }
+            commands::onchain_demo::onchain_demo(&mut cfg, d).await;
+            db.ceremonies().save_ceremony(cfg)?;
+        }
+        _ => unreachable!(),
     }
     Ok(())
 }
+// cargo run -- vote --amount 1 --vote 1 --revote 0 --private-key 0xfc8b5067256b891cb8d078cd657805ab62ef75888506104acf0b0001ea5c349c
