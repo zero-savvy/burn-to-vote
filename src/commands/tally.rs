b42de06f238e121f55b277e3c07d30626c90eccf
pardis-toolabi <pardis.tlbi@gmail.com>
2025-06-06 11:14:50 +0200

    clean up

--- src/commands/tally.rs
+++ src/commands/tally.rs
@@ -1,11 +1,11 @@
 use crate::utils::config::{get_time_stamp, Config};
+use log::info;
+use structopt::StructOpt;
 use ethers::{
     providers::{self, Http, Middleware, Provider},
     types::BlockId,
 };
-use log::info;
 use primitive_types::U256;
-use structopt::StructOpt;
 #[derive(Debug, StructOpt, Clone)]
 pub struct Tally {
     ceremoni_id: u64,
@@ -15,21 +15,24 @@ pub async fn tally(
     ceremony_id: Tally,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let provider: Provider<Http> = Provider::<Http>::try_from(config.clone().network.url())
-        .expect("Error: failed to initiate provider.");
-    let current_ts: primitive_types::U256 = get_time_stamp(&provider).await;
-    let tally_deadline = config.clone().tallyDeadline.unwrap();
+    .expect("Error: failed to initiate provider.");
+    let current_ts:primitive_types::U256 = get_time_stamp(&provider).await;
+    let tally_deadline = config
+    .clone()
+    .tallyDeadline
+    .unwrap();
     let voting_ts = U256::from_dec_str(&tally_deadline)?;
 
     if current_ts > voting_ts.into() {
         let yesVotes = config.clone().yesVotes;
         let noVotes = config.clone().noVotes;
-        info!("Yes vote counts: {}", yesVotes);
-        info!("No vote counts: {}", noVotes);
+        info!("Yes vote counts: {:?}", yesVotes);
+        info!("No vote counts: {:?}", noVotes);
         config.finilized = true;
 
         if yesVotes > noVotes {
             config.result = Some(true)
-        } else {
+        }else {
             config.result = Some(false)
         }
 
