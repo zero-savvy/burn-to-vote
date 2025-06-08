4890734154ccdd68a4ca142f983ebb77ddbd0819
pardis-toolabi <pardis.tlbi@gmail.com>
2025-06-08 18:20:53 +0200

    update deploy script

--- contracts/script/Voting.s.sol
+++ contracts/script/Voting.s.sol
@@ -11,24 +11,23 @@ contract VotingScript is Script {
 
     function setUp() public {}
 
-    function run() public {
+    function run(
+        uint256 votingTime,
+        uint256 tallyTime,
+        uint256 ceremony_id,
+        uint256 mt,
+        uint256 state_root
+    ) public {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privateKey);
 
-        // uint256 base_time = 1000;
-        // vm.warp(base_time);
-
-        uint256 votingTime = block.timestamp + 10000;
-        uint256 tallyTime = block.timestamp + 20000;
-        uint256 ceremeny_id = 16325618567567054338;
-        uint256 mt = 6279442489816579343175600576641714715845361010123760250696645575814262324581;
-        uint state_root = 9133689217370487228376476215699836963181592635914481284078419964281904630813;
+        uint256 b = vm.addr(privateKey).balance;
+        console.log("balance", b);
 
         verifier = new Groth16Verifier();
-        voting = new Voting(address(verifier), votingTime, tallyTime, mt, ceremeny_id, state_root);
+        voting = new Voting(address(verifier), votingTime, tallyTime, mt, ceremony_id, state_root);
 
         vm.stopBroadcast();
         console.logAddress(address(voting));
     }
 }
-// forge script VotingScript --rpc-url http://localhost:8545 --broadcast
