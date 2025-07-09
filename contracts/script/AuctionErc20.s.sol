d0afe0c22117181fd32139d4cdafe4d835cad6ca
pardis-toolabi <pardis.tlbi@gmail.com>
2025-07-09 14:17:29 +0200

    update token script

--- contracts/script/AuctionErc20.s.sol
+++ contracts/script/AuctionErc20.s.sol
@@ -2,37 +2,30 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Erc20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AuctionErc20} from "../src/AuctionErc20.sol";
 
-contract AuctionErc20 is Script {
-    Erc20 public token;
+contract TokenScript is Script {
+    AuctionErc20 public auctionErc20;
 
     function setUp() public {}
 
-    function run(
-        string name,
-        string symbol
-    ) public {
+    function run(address[] memory _whiteList) public {
         uint256 privateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(privateKey);
 
         uint256 b = vm.addr(privateKey).balance;
         console.log("balance", b);
 
-        verifier = new Groth16Verifier();
-        votingFactory = new VotingFactory();
-        address voting = votingFactory.deployVotingContract(
-            salt,
-            VotingFactory.CeremonyType.Binary,
-            address(verifier),
-            submissionDeadline,
-            tallyDeadline,
-            merkleRoot,
-            ceremonyId,
-            stateRoot
-        );
+        auctionErc20 = new AuctionErc20(_whiteList);
 
         vm.stopBroadcast();
-        console.logAddress(address(voting));
+        console.logAddress(address(auctionErc20));
+
+        for (uint256 i = 0; i < _whiteList.length; i++) {
+            uint256 tokenBalance = auctionErc20.balanceOf(_whiteList[i]);
+            console.log("Token balance of", _whiteList[i], ":", tokenBalance);
+        }
     }
 }
+
+// cd contracts && forge script TokenScript --rpc-url http://127.0.0.1:8545 --broadcast --sig 'run(address[])' [0x670F1836dfe9649c4C953721A4000a9858aBbDD9,0xe860c028D17501584c1f2A93f19E482132907D7A]
