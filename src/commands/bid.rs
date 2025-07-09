5a5f17c389d3fb22494d11d2ae76f48d762cacbf
pardis-toolabi <pardis.tlbi@gmail.com>
2025-07-09 13:35:01 +0200

    adding bidding command base

---
+++ src/commands/bid.rs
@@ -0,0 +1,4 @@
+// auction
+// 1. initiare auction starts with the token distribiutions
+// contract is deployes after the start of the project 
+// and after the last block of voting since we need the state root of the last blick
